33b7ac40a82f2e5e70d4348cfde224bf057e2fdd
SAMEEP DAS <sameepdasxi@gmail.com>
2026-01-22 17:42:34 +0530

    Optimised the FLIP THE COIN game by adding more secured user input fetching, faster DOM updates, more modularised code and seperated computation from rendering. Followed compute first then render logic

--- scripts/flip-the-coin.js
+++ scripts/flip-the-coin.js
@@ -1,151 +1,203 @@
-import {getElementByClass, removeClass, addClass, removePropByClass, addClassByID, removeClassByID, changeText, setPropByClass} from './utils/dom_manipulation.js';
+import {
+  getElementByClass,
+  removeClass,
+  addClass,
+  removePropByClass,
+  addClassByID,
+  removeClassByID,
+  changeText,
+  setPropByClass,
+} from "./utils/dom_manipulation.js";
 
-const onReset = new Audio('../resources/welcome.mp3');
-const onTouch = new Audio('../resources/onTouch.mp3');
+import { getRandomInt } from "./utils/randomness.js";
 
-let headInput = getElementByClass('js-head-input');
-let tailInput = getElementByClass('js-tail-input');
+import {
+  TOSS,
+  popupAnimationDelayMS,
+  autoPlayDelayMS,
+} from "./utils/constants.js";
 
-let score = JSON.parse(localStorage.getItem('ftpScore'));
+//THE AUDIO FILES
+const onReset = new Audio("../resources/welcome.mp3");
+const onTouch = new Audio("../resources/onTouch.mp3");
+
+//LOCAL STORAGE SAVES AN OBJECT CALLED SCORE WITH
+// OBJECT NAME = SCORE AND MEMBER IS WINS, INITIALLY SET TO 0
+let score = JSON.parse(localStorage.getItem("ftpScore"));
 if (!score) {
   score = {
-    wins: 0
+    wins: 0,
   };
 }
 
-changeText('score', score.wins);
+//-------------EVENT LISTENER LIST--------------//
 
-function compChoice() {
-  let randomNo = Math.random();
-  return randomNo;
+//GET THE INPUT FROM THE USER (HEADS OR TAILS)
+
+let tailInput = getElementByClass("js-tail-input");
+//LISTEN TO THE USER INPUT FOR TAILS
+tailInput.addEventListener("click", () => {
+  const compMove = defineToss(getRandomInt(0, 1));
+  let result = getResult(TOSS.TAILS, compMove);
+  updateDOMByBatch(compMove, result);
+  onTouch.play();
+});
+
+let headInput = getElementByClass("js-head-input");
+//LISTEN TO THE USER INPUT FOR HEADS
+headInput.addEventListener("click", () => {
+  const compMove = defineToss(getRandomInt(0, 1));
+  let result = getResult(TOSS.HEADS, compMove);
+  updateDOMByBatch(compMove, result);
+  onTouch.play();
+});
+
+//LISTEN TO THE RESET BUTTON
+let resetBtn = getElementByClass("reset-btn");
+resetBtn.addEventListener("click", () => {
+  handleReset();
+});
+
+//LISTEN TO THE AUTO PLAY BUTTON
+let autoPlayBtn = getElementByClass("js-auto-play");
+autoPlayBtn.addEventListener("click", () => {
+  handleAutoPlay();
+});
+
+//EACH TIME PAGE IS RERENDERED THE SCORE IS UPDATED FROM THE LOCAL STORAGE DATA
+changeText("score", score.wins);
+
+//COMPUTER'S CHOICE IS CONVERTED INTO HEADS AND TAILS ACCORDINGLY
+function defineToss(compMove) {
+  return compMove === 0 ? TOSS.HEADS : TOSS.TAILS;
 }
 
-function defineToss(value) {
-  if (value >= 0 && value <= 0.5) {
-    return "Head";
-  }
-  else if (value > 0.5 && value <= 1) {
-    return "Tail";
+//BATCH DOM UPDATES
+const updateDOMByBatch = (compMove, result) => {
+  renderComputerMove(compMove);
+  renderResultEffects(result);
+  renderResultPopup(result);
+};
+
+//FUNCTION TO RENDER THE COMPUTER'S CHOICE ON THE SCREEN
+function renderComputerMove(computerMove) {
+  if (computerMove === TOSS.HEADS) {
+    removeClass("js-output-icon", "tail-icon");
+    addClass("js-output-icon", "head-icon");
+  } else {
+    removeClass("js-output-icon", "head-icon");
+    addClass("js-output-icon", "tail-icon");
   }
 }
 
-function evaluateResult(userChoice) {
-  let computerMove = defineToss(compChoice());
-  if(computerMove === 'Head'){
-    removeClass('js-output-icon', 'tail-icon');
-    addClass('js-output-icon', 'head-icon');
-  } else{
-    removeClass('js-output-icon', 'head-icon');
-    addClass('js-output-icon', 'tail-icon');
-  }
-  
-  removeClass('toss-result', 'emote-disabled');
-  if (computerMove === userChoice) {
-    addClass('toss-result', 'green-result');
-    changeText('toss-result', 'Victory!');
-    setTimeout(() => {
-      removeClass('victory-popup', 'show-popup');
-      }, 700);
-    addClass('victory-popup', 'show-popup');
+//FUNCTION TO UPDATE THE SCORE AND UPDATE THE OBJECT IN THE LOCAL STORAGE
+function updateScore(isVictory) {
+  if (!isVictory && score.wins > 0) {
+    score.wins -= 1;
+  } else {
     score.wins += 1;
   }
-  else {
-    removeClass('toss-result', 'green-result');
-    changeText('toss-result', 'Oops! Defeat');
+  localStorage.setItem("ftpScore", JSON.stringify(score));
+  changeText("score", score.wins);
+}
+
+//FUNCTION TO RENDER THE ANIMATION POPUP ACCORDING TO THE RESULT
+function renderResultPopup(isVictory) {
+  if (isVictory) {
     setTimeout(() => {
-        removeClass('defeat-popup', 'show-popup');
-      }, 700);
-    addClass('defeat-popup', 'show-popup');
-    if (score.wins > 0) {
-      score.wins -= 1;
-    }
+      removeClass("victory-popup", "show-popup");
+    }, popupAnimationDelayMS);
+    addClass("victory-popup", "show-popup");
+    updateScore(true);
+  } else {
+    setTimeout(() => {
+      removeClass("defeat-popup", "show-popup");
+    }, popupAnimationDelayMS);
+    addClass("defeat-popup", "show-popup");
+    updateScore(false);
   }
-  localStorage.setItem('ftpScore', JSON.stringify(score));
-  changeText('score', score.wins);
 }
 
-tailInput
-  .addEventListener('click',
-    () => {
-      evaluateResult("Tail");
-      onTouch.play();
-    });
-
-headInput
-  .addEventListener('click',
-    () => {
-      evaluateResult("Head");
-      onTouch.play();
-    });
-
-function removeFeatures(){
-  removePropByClass('js-head-input', 'opacity');
-  removePropByClass('js-tail-input', 'opacity');
-  removeClass('js-head-input', 'choice-select');
-  removeClass('js-tail-input', 'choice-select');
+//COMPUTES THE RESULT //SIMPLE COMAPRISON
+function getResult(userChoice, computerMove) {
+  return computerMove === userChoice ? true : false;
 }
 
+//RENDERS THE SCREEN AS PER THE RESULT
+function renderResultEffects(result) {
+  removeClass("toss-result", "emote-disabled");
+  if (result) {
+    addClass("toss-result", "green-result");
+    changeText("toss-result", "Victory!");
+  } else {
+    removeClass("toss-result", "green-result");
+    changeText("toss-result", "Oops! Defeat");
+  }
+}
+
+//REMOVE THE SIDE EFFECTS WHEN PAUSE OR RESET IS CLICKED
+function updateSideEffectsOnReset() {
+  removePropByClass("js-head-input", "opacity");
+  removePropByClass("js-tail-input", "opacity");
+  removeClass("js-head-input", "choice-select");
+  removeClass("js-tail-input", "choice-select");
+}
+
+//HANDLE THE RESET FUNCTIONALITY
 function handleReset() {
-  onReset.play();
-  removeFeatures();
-  localStorage.removeItem('ftpScore');
-  score = JSON.parse(localStorage.getItem('ftpScore'));
+  onReset.play(); //PLAY MUSIC
+  updateSideEffectsOnReset(); //REMOVE THE SIDE EFFECTS OF PREVIOUS RENDERS
+  localStorage.removeItem("ftpScore"); //UPDATE LOCAL STORAGE ITEM TO NULL
+  score = JSON.parse(localStorage.getItem("ftpScore")); //CREATE ITEM
   if (score === null) {
+    //SET ITEM
     score = {
-      wins: 0
+      wins: 0,
     };
   }
-  
-  addClass('toss-result', 'emote-disabled');
-  removeClass('js-output-icon', 'head-icon');
-  removeClass('js-output-icon', 'tail-icon');
-  changeText('score', score.wins);
+  //REMOVE SIDE EFFECTS IF ANY
+  addClass("toss-result", "emote-disabled");
+  removeClass("js-output-icon", "head-icon");
+  removeClass("js-output-icon", "tail-icon");
+  changeText("score", score.wins);
 }
 
-let resetBtn = getElementByClass('reset-btn');
-resetBtn
-  .addEventListener('click',
-    () => {
-      handleReset();
-    });
-
-let autoPlayBtn = getElementByClass('js-auto-play');
-autoPlayBtn
-  .addEventListener('click',
-    () => {
-      handleAutoPlay();
-    });
-
+//FUNCTION TO RENDER COMPUTER AS AN USER
+function renderComputerAsUser(computerMove) {
+  if (computerMove === TOSS.HEADS) {
+    setPropByClass("js-tail-input", "opacity", "0.7"); //DIMS THE OPPOSITE OPTION TO CHOICE
+    removePropByClass("js-head-input", "opacity"); //REMOVES PREVIOUS DIMMED EFFECT ON CURR CHOICE
+    removeClass("js-tail-input", "choice-select"); //REMOVES PREVIOUS SELECT BORDER ON OPPOSITE OPTION
+    addClass("js-head-input", "choice-select"); //HIGHLIGHTS THE CURRENT CHOICE
+  } else {
+    setPropByClass("js-head-input", "opacity", "0.7");
+    removePropByClass("js-tail-input", "opacity");
+    removeClass("js-head-input", "choice-select");
+    addClass("js-tail-input", "choice-select");
+  }
+  onTouch.play(); //PLAYS THE AUDIO
+}
 
 let isAutoPlay = false;
 let intervalID;
-function handleAutoPlay(){
-  if(!isAutoPlay){
-    changeText('js-auto-play', 'Pause Play');
+//HANDLE THE AUTO-PLAY FUNCTIONALITY
+function handleAutoPlay() {
+  if (!isAutoPlay) {
+    changeText("js-auto-play", "Pause Play");
     intervalID = setInterval(() => {
-      let compAsUserMove = defineToss(compChoice());
-      if(compAsUserMove === 'Head'){
-        setPropByClass('js-tail-input', 'opacity', '0.7');
-        removePropByClass('js-head-input','opacity');
-        removeClass('js-tail-input', 'choice-select');
-        addClass('js-head-input', 'choice-select');
-        onTouch.play();
-      }
-      else if(compAsUserMove === 'Tail'){
-        setPropByClass('js-head-input', 'opacity', '0.7');
-        removePropByClass('js-tail-input','opacity');
-        removeClass('js-head-input', 'choice-select');
-        addClass('js-tail-input', 'choice-select');
-        onTouch.play();
-      }
-      evaluateResult(compAsUserMove);
-    }, 2000);
-    isAutoPlay = true;
-  }
-  else{
-    removeFeatures();
-    changeText('js-auto-play', 'Auto Play');
+      let compAsUserMove = defineToss(getRandomInt(0, 1));
+      renderComputerAsUser(compAsUserMove);
+      let compMove = defineToss(getRandomInt(0, 1));
+      renderComputerMove(compMove);
+      let result = getResult(compAsUserMove, compMove);
+      renderResultEffects(result);
+      renderResultPopup(result);
+    }, autoPlayDelayMS);
+    isAutoPlay = !isAutoPlay;
+  } else {
+    updateSideEffectsOnReset();
+    changeText("js-auto-play", "Auto Play");
     clearInterval(intervalID);
-    isAutoPlay = false;
+    isAutoPlay = !isAutoPlay;
   }
 }
---
+++ scripts/utils/constants.js
@@ -0,0 +1,14 @@
+export const TOSS = Object.freeze({
+  HEADS: "HEADS",
+  TAILS: "TAILS",
+});
+
+export const GAME_MODES = Object.freeze({
+  MEDIUM : "MEDIUM",
+  EASY : "EASY",
+  HARD : "HARD",
+});
+
+export const popupAnimationDelayMS = 700;
+
+export const autoPlayDelayMS = 1800;
